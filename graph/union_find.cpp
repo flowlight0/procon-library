e381fe28e025cad04a47ed75488f5caf9ae29cd3
takanori hayashi <flowlight0@gmail.com>
2013-12-14 17:36:50 +0900

    Add strong_connected_component.cpp

--- graph/union_find.cpp
+++ graph/union_find.cpp
@@ -14,43 +14,42 @@ http://www.spoj.com/problems/MST/ (only find, same and unite are used.)
 using namespace std;
 
 class UnionFind{
-  vector<int> parent;
-  vector<int> count;
-  vector<int> rank;
+    vector<int> parent;
+    vector<int> count;
+    vector<int> rank;
 public:
-  
-  UnionFind(int N) : parent(vector<int>(N)),
-                     count(vector<int>(N, 1)),
-                     rank(vector<int>(N, 0)){
-    for(int i = 0; i < N; i++) parent[i] = i;
-  }
-  
-  int find(int x){
-    if(x == parent[x]) return x;
-    else return parent[x] = find(parent[x]);
-  }
-  
-  int weight(int x){
-    return count[find(x)];
-  }
-  
-  bool same(int x, int y){
-    return find(x) == find(y);
-  }
-
-  void unite(int x, int y){
-    x = find(x);
-    y = find(y);
-    if(x == y) return;
-    if(rank[x] < rank[y]){
-      count[y] += count[x];
-      parent[x] = y;
-    }else{
-      count[x] += count[y];
-      parent[y] = x;
-      if(rank[x] == rank[y]) rank[y]++;
+    UnionFind(int N) : parent(vector<int>(N)),
+                       count(vector<int>(N, 1)),
+                       rank(vector<int>(N, 0)){
+        for(int i = 0; i < N; i++) parent[i] = i;
     }
-  }
+  
+    int find(int x){
+        if(x == parent[x]) return x;
+        else return parent[x] = find(parent[x]);
+    }
+  
+    int weight(int x){
+        return count[find(x)];
+    }
+  
+    bool same(int x, int y){
+        return find(x) == find(y);
+    }
+    
+    void unite(int x, int y){
+        x = find(x);
+        y = find(y);
+        if(x == y) return;
+        if(rank[x] < rank[y]){
+            count[y] += count[x];
+            parent[x] = y;
+        }else{
+            count[x] += count[y];
+            parent[y] = x;
+            if(rank[x] == rank[y]) rank[y]++;
+        }
+    }
 };
 
 
@@ -60,41 +59,41 @@ public:
 #include <algorithm>
 
 struct edge{
-  int from, to, cost;
-  edge(){}
-  edge(int from, int to, int cost) : from(from), to(to), cost(cost){}
+    int from, to, cost;
+    edge(){}
+    edge(int from, int to, int cost) : from(from), to(to), cost(cost){}
   
-  friend bool operator<(const edge &e1, const edge &e2){
-    return e1.cost < e2.cost;
-  }
+    friend bool operator<(const edge &e1, const edge &e2){
+        return e1.cost < e2.cost;
+    }
 };
 
 
 int main(){
-  int N, M;
-  scanf("%d%d", &N, &M);
+    int N, M;
+    scanf("%d%d", &N, &M);
   
-  vector<edge> es(M);
+    vector<edge> es(M);
   
-  for(int i = 0; i < M; i++){
-    scanf("%d%d%d", &es[i].from, &es[i].to, &es[i].cost);
-    es[i].from--;
-    es[i].to--;
-  }
-  sort(es.begin(), es.end());
-  
-  long long res = 0;
-
-  UnionFind uf(N);
-	       
-  for(int i = 0; i < M; i++){
-    if(!uf.same(es[i].from, es[i]. to)){
-      res += es[i].cost;
-      uf.unite(es[i].from, es[i]. to);
+    for(int i = 0; i < M; i++){
+        scanf("%d%d%d", &es[i].from, &es[i].to, &es[i].cost);
+        es[i].from--;
+        es[i].to--;
     }
-  }
+    sort(es.begin(), es.end());
   
-  printf("%lld\n", res);
+    long long res = 0;
+
+    UnionFind uf(N);
+	       
+    for(int i = 0; i < M; i++){
+        if(!uf.same(es[i].from, es[i]. to)){
+            res += es[i].cost;
+            uf.unite(es[i].from, es[i]. to);
+        }
+    }
   
-  return 0;
+    printf("%lld\n", res);
+  
+    return 0;
 }
--- string/Z.cpp
+++ string/Z.cpp
@@ -1,71 +1,68 @@
-// Z algorithm
-// 与えられた文字列Sの各0 <= i < |S|に対して
-// S[i...i+k) == S[0...k)となる最大のkをO(|S|)求める。
+/***********************************************************
+ Z algorithm
+ 与えられた文字列Sの各0 <= i < |S|に対して
+ S[i...i+k) == S[0...k)となる最大のkをO(|S|)求める。
 
-// verified at
-// http://jag2013summer-day3.contest.atcoder.jp/tasks/icpc2013summer_day3_h
-  
-// 参考文献
-// http://codeforces.com/blog/entry/3107
-
+ verified at
+ http://jag2013summer-day3.contest.atcoder.jp/tasks/icpc2013summer_day3_h
+ 
+ 参考文献
+ http://codeforces.com/blog/entry/3107
+***********************************************************/
 
 #include <vector>
 #include <string>
 
 class ZIndex{
-  std::vector<int> z;
+    std::vector<int> z;
 public:
-  ZIndex(const std::string &S) : z(S.size()){
-    int L = 0, R = 0;
-    int n = S.size();
-    
-    for(int i = 1; i < n; i++){
-      if(i > R){
-	L = R= i;
-	while(R < n && S[R-L] == S[R]) R++;
-	z[i] = R - L; R--;
-      }else if(z[i - L] < R - i + 1){
-	z[i] = z[i - L];
-      }else{
-	L = i;
-	while(R < n && S[R-L] == S[R]) R++;
-	z[i] = R - L; R--;
-      }
+    ZIndex(const std::string &S) : z(S.size()){
+        int L = 0, R = 0;
+        int n = S.size();
+        for(int i = 1; i < n; i++){
+            if(i > R){
+                L = R= i;
+                while(R < n && S[R-L] == S[R]) R++;
+                z[i] = R - L; R--;
+            }else if(z[i - L] < R - i + 1){
+                z[i] = z[i - L];
+            }else{
+                L = i;
+                while(R < n && S[R-L] == S[R]) R++;
+                z[i] = R - L; R--;
+            }
+        }
+        z[0] = S.size();
     }
-    z[0] = S.size();
-  }
-  int operator[](int pos) const { return z[pos]; }
+    int operator[](int pos) const { return z[pos]; }
 };
 
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
-
-
 int main(){
-  string S, T;
-  cin >> S >> T;
-  ZIndex lz(T + '$' + S);
-  reverse(S.begin(), S.end());
-  reverse(T.begin(), T.end());
-  ZIndex rz(T + '$' + S);
+    string S, T;
+    cin >> S >> T;
+    ZIndex lz(T + '$' + S);
+    reverse(S.begin(), S.end());
+    reverse(T.begin(), T.end());
+    ZIndex rz(T + '$' + S);
 
-  int N = S.size(), M = T.size();
+    int N = S.size(), M = T.size();
   
-  vector<int> L(N);
-  vector<int> R(N);
-  for(int i = M + 1; i <= N + M; i++){
-    L[i-M-1] = lz[i];
-    R[i-M-1] = rz[i];
-  }
-
-  int res = 0;
-  for(int i = 0; i < N - M + 1; i++){
-    if(L[i] + R[N-i-M] == M - 1) res++;
-  }
-  cout << res << endl;
-  return 0;
+    vector<int> L(N);
+    vector<int> R(N);
+    for(int i = M + 1; i <= N + M; i++){
+        L[i-M-1] = lz[i];
+        R[i-M-1] = rz[i];
+    }
+    
+    int res = 0;
+    for(int i = 0; i < N - M + 1; i++){
+        if(L[i] + R[N-i-M] == M - 1) res++;
+    }
+    cout << res << endl;
+    return 0;
 }
-
 
